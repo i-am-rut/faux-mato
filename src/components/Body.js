2a862f44387e05ab0ce3f1c78f289fc9eb3599f7
Amrut Patil <patilamrut578@gmail.com>
2025-03-05 21:10:39 +0530

    Add search and filter to restaurants and fix cors problem

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,11 @@
 import ReastaurantCard from "./RestaurantCard"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import ShimmerResCards from "./ShimmerResCards"
 
 const Body = () => {
     const [resList, setResList] = useState([])
+    const [filteredList, setFilteredList] = useState([])
+    const search = useRef(null)
 
 
     useEffect(() => {
@@ -11,17 +13,22 @@ const Body = () => {
     }, [])
 
     const fetchData = async() => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const data = await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
 
         const json = await data.json()
         setResList(json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map(res => res.info))
+        setFilteredList(json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map(res => res.info))
         
     }
 
+    const handleSearchClick = () => {
+        const filtered = resList.filter(res => res.name.toLowerCase().includes(search.current.value.toLowerCase()))
+        setFilteredList(filtered)
+    }
+
     const handleTopResClick = () => {
-        const filtered = resList.filter(res => res.avgRating > 4.4)
-        // console.log(filtered)
-        setResList(filtered)
+        const filtered = filteredList.filter(res => res.avgRating > 4.4)
+        setFilteredList(filtered)
     }
 
     
@@ -29,13 +36,18 @@ const Body = () => {
         <div className="body-container">
             <div className="filter-container">
                 <div className="search-container">
-                    <input name="search-input" type="text" />
-                    <button>Search</button>
+                    <input 
+                        ref={search} 
+                        name="search-input" 
+                        type="text"  
+                        onKeyUp={(e => e.key === "Enter" && handleSearchClick())}
+                        placeholder="Search restaurants"/>
+                    <button onClick={handleSearchClick}>Search</button>
                 </div>
                 <button onClick={handleTopResClick} className="filter-button">Top Rated Restaurants</button>
             </div>
             <div className="res-card-container">
-                {resList.length === 0 ? <ShimmerResCards /> : resList.map(res => <ReastaurantCard key={res.id} resData={res} />)
+                {resList.length === 0 ? <ShimmerResCards /> : filteredList.length === 0? <h1>No results matching applied filters.</h1> : filteredList.map(res => <ReastaurantCard key={res.id} resData={res} />)
                 }
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
+
 const Header = () => {
+    const [logStatusButton, setLogStatusButton] = useState("Login")
+
+    const handleLogStatusButtonClick = () => {
+        setLogStatusButton(prev => prev === "Login" ? "Logout" : "Login")
+    }
+
     return (
         <div className="header-container">
             <img src="https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg" alt="faux-mato-logo" className="faux-mato-logo" />
@@ -7,6 +16,7 @@ const Header = () => {
                 <li>About</li>
                 <li>Contact</li>
                 <li>Cart</li>
+                <button className="login-logout-button" onClick={handleLogStatusButtonClick}>{logStatusButton}</button>
             </ul>
         </div>
     )
